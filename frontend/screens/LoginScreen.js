8e6bbe24952cd69a0c5ce44dc9a57b535da2e64c
Magnus Axelsen <maxels19@student.aau.dk>
2023-03-08 15:47:32 +0100

    created basic login UI

--- frontend/screens/LoginScreen.js
+++ frontend/screens/LoginScreen.js
@@ -1,12 +1,75 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 
 export default function LoginScreen() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <View>
-            <Text>LoginScreen</Text>
-        </View>
+        <KeyboardAvoidingView style={styles.container} behavior="height">
+            <View style={styles.inputContainer}>
+                <TextInput placeholder='Email' style={styles.input} value={email} onChangeText={text => setEmail(text)} />
+                <TextInput placeholder='Password' value={password} onChangeText={text => setPassword(text)} style={styles.input} secureTextEntry />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, styles.buttonOutline]}>
+                    <Text style={styles.buttonOutlineText}>Register</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    inputContainer: {
+        width: '80%'
+    },
+    input: {
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+        backgroundColor: "white",
+    },
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    },
+
+    button: {
+        backgroundColor: 'blue',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: "white",
+        fontWeight: 800,
+        fontSize: 16,
+    },
+
+    buttonOutline: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: "blue",
+        borderWidth: 2,
+    },
+    buttonOutlineText: {
+        color: "blue",
+        fontWeight: 800,
+        fontSize: 16,
+    }
+})
