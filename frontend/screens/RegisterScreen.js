03fe0cedf5d3f5677188360e9512c1728200a51e
Magnus Axelsen <maxels19@student.aau.dk>
2023-03-13 20:40:31 +0100

    Added new landing page

--- frontend/screens/RegisterScreen.js
+++ frontend/screens/RegisterScreen.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { auth } from '../firebase';
 import { useNavigation } from '@react-navigation/native';
 
-export default function LoginScreen() {
+export default function RegisterScreen() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -21,21 +21,20 @@ export default function LoginScreen() {
 
 
     const handleSignUp = () => {
-        /*auth.createUserWithEmailAndPassword(email, password)
-            .then(userCredentials => {
-                const user = userCredentials.user;
-                console.log(user.email);
-            }).catch(error => alert(error.message))*/
-
-        navigation.navigate("Register");
-    }
-
-    const handleLogin = () => {
-        auth.signInWithEmailAndPassword(email, password)
+        auth.createUserWithEmailAndPassword(email, password)
             .then(userCredentials => {
                 const user = userCredentials.user;
                 console.log(user.email);
             }).catch(error => alert(error.message))
+    }
+
+    const handleLogin = () => {
+        /* auth.signInWithEmailAndPassword(email, password)
+             .then(userCredentials => {
+                 const user = userCredentials.user;
+                 console.log(user.email);
+             }).catch(error => alert(error.message))
+             */
     }
 
     return (
@@ -46,7 +45,7 @@ export default function LoginScreen() {
             </View>
             <View style={styles.buttonContainer}>
                 <TouchableOpacity onPress={handleLogin} style={styles.button}>
-                    <Text style={styles.buttonText}>Login</Text>
+                    <Text style={styles.buttonText}>Create Account</Text>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.buttonOutline]}>
                     <Text style={styles.buttonOutlineText}>Register</Text>
