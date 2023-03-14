7176bfd6005a137e811958f0a309a1b2f49bd683
Thomas Grubbe Sandborg Lauritsen <36702062+Sandborg@users.noreply.github.com>
2023-03-14 20:16:19 +0100

    feature #2: Everything set up
    Everything is followed from guides, but the problem is now with the cmake build - it seems like they can't find the pybind11 libraries.

---
+++ Server/algorithms/algo_functions/test_file.h
@@ -0,0 +1,14 @@
+//
+// Created by Sandborg on 14/03/2023.
+//
+
+
+#ifndef ALGORITHMS_TEST_FILE_H
+#define ALGORITHMS_TEST_FILE_H
+#include "cmath"
+
+int add(int i, int j);
+int sub(int i, int j);
+int square(int i);
+void hello();
+#endif //ALGORITHMS_TEST_FILE_H
