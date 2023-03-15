b5cc40b78cc4b7986e8878416127868d8ed98337
Magnus Axelsen <maxels19@student.aau.dk>
2023-03-15 13:36:12 +0100

    Created simple connection between React Nattive application and API

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
-
+from flask import request
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_magnus():
     return "<p> Hello, Magnus :)!</p>"
+
+
+@app.route("/worker/state", methods = ['POST'])
+def updateState():
+    data = request.data
+    print(data)
+    return data
+    
+app.run('0.0.0.0', port=5000)
