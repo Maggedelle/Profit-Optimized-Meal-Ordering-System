b93155a5ae737845cd6e82748993abd26291e871
Sandborg <Laurits1994@gmail.com>
2023-04-04 11:59:33 +0200

    feature #22: exposed Order and Courier class to the python side.

--- Server/algorithms/test.py
+++ Server/algorithms/test.py
@@ -3,4 +3,9 @@ sys.path.append('build')
 
 import profit_module as pm
 
+courier_test = pm.Courier("Magnus",0,0,0,0)
+order = pm.Order("Magnus1",0,0,0,0,0,0,0)
+print(order)
+print(courier_test, courier_test.latitude, courier_test.longitude, courier_test.speed, courier_test.max_distance)
+
 print(pm.hello2())
